46703634b9a4d6240cb01b5df32cc51126ebaa44
Eryk-Szymanski <e.szymanski.33@interia.pl>
2023-06-02 08:52:19 +0200

    Wszystkie widoki są responsywne;

--- views/new-order.php
+++ views/new-order.php
@@ -13,57 +13,66 @@
     ?>
   </head>
   <body>
-    <div class="container-fluid w-100 bg-dark screen-height d-flex justify-content-center text-light menu-buffer">
+    <div class="container-fluid w-100 bg-dark screen-height d-flex justify-content-center text-light menu-buffer py-4">
       <?php if (isset($_SESSION['success'])) : ?>
         <?php require_once './components/menu.php'; ?>
-        <div class="col col-lg-6 p-4 mx-1 my-4 bg-warning bg-gradient rounded d-flex flex-column justify-content-center align-items-center">
-          <h3>Nowe zamówienie</h3>
+        <div class="col col-lg-6 p-4 mx-1 my-4 bg-warning bg-gradient rounded d-flex flex-column">
+          <h3 class="px-4 py-2">Nowe zamówienie</h3>
 
-          <form action="../scripts/new-order.php" method="post">
+          <form action="../scripts/new-order.php" method="post" class="w-100">
             <?php
 
 // Informacje o kliencie
               $sql = "SELECT id, name, surname, email, phone, address_id FROM `users` WHERE id = $_SESSION[user_id]";
               $result = $mysqli->query($sql);
               $user = $result->fetch_assoc();
-              echo <<< USER_DATA
-                <input type="number" value="$user[id]" name="user_id" hidden />
-                <input type="number" value="$user[address_id]" name="address_id" hidden />
-                <h5>Imię: $user[name]</h5>
-                <h5>Nazwisko: $user[surname]</h5>
-                <h5>Email: $user[email]</h5>
-                <h5>Telefon: $user[phone]</h5>
-USER_DATA;
 
 // Informacje o adresie dostawy
               $sql = "SELECT zipcode, city, street, apartment FROM `addresses` WHERE id = $user[address_id]";
               $result = $mysqli->query($sql);
               $address = $result->fetch_assoc();
-              echo <<< USER_ADDRESS
-                <h5>Kod pocztowy: $address[zipcode]</h5>
-                <h5>Miasto: $address[city]</h5>
-                <h5>Ulica: $address[street]</h5>
-                <h5>Budynek/mieszkanie: $address[apartment]</h5>
-USER_ADDRESS;
+              echo <<< USER_DATA
+                <div class="d-flex flex-column flex-lg-row">
+                  <div class="col col-lg-6 p-4 border-top border-white">
+                    <input type="number" value="$user[id]" name="user_id" hidden />
+                    <input type="number" value="$user[address_id]" name="address_id" hidden />
+                    <h3>Dane</h3>
+                    <h5>Imię: $user[name]</h5>
+                    <h5>Nazwisko: $user[surname]</h5>
+                    <h5>Email: $user[email]</h5>
+                    <h5>Telefon: $user[phone]</h5>
+                  </div>
+                  <div class="col col-lg-6 p-4 border-top border-white">
+                    <h3>Adres</h3>
+                    <h5>Kod pocztowy: $address[zipcode]</h5>
+                    <h5>Miasto: $address[city]</h5>
+                    <h5>Ulica: $address[street]</h5>
+                    <h5>Budynek/mieszkanie: $address[apartment]</h5>
+                  </div>
+                </div>
+USER_DATA;
 
 // Metoda płatności
               $sql = "SELECT * FROM `payment_methods`";
               $result = $mysqli->query($sql);
+              echo "<div class='d-flex flex-column flex-lg-row'>";
+              echo "<div class='col col-lg-6 p-4 border-top border-white'>";
               echo "<h5>Wybierz metodę płatności:</h5>";
               while($payment_method = $result->fetch_assoc()) {
-                echo <<< PAYMENT_METHOD
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="paymentMethod" id="paymentMethod" value="$payment_method[id]">
-                  <label class="form-check-label" for="paymentMethod">
-                    $payment_method[name]
-                  </label>
-                </div>
+                echo <<<PAYMENT_METHOD
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="paymentMethod" id="paymentMethod" value="$payment_method[id]">
+                    <label class="form-check-label" for="paymentMethod">
+                      $payment_method[name]
+                    </label>
+                  </div>
 PAYMENT_METHOD;
               }
-
+              echo "</div>";
 // Sposób dostawy  
               $sql = "SELECT * FROM `delivery_methods`";
               $result = $mysqli->query($sql);
+              echo "<div class='col col-lg-6 p-4 border-top border-white'>";
               echo "<h5>Wybierz sposób dostawy:</h5>";
               while($delivery_method = $result->fetch_assoc()) {
                 echo <<< DELIVERY_METHOD
@@ -76,20 +85,24 @@ PAYMENT_METHOD;
 DELIVERY_METHOD;
               }
             ?>
-            
-            <input type="text" class="form-control" placeholder="Dodaj komentarz" name="comments">
-
-            <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-              <input type="checkbox" class="btn-check" id="agreeTerms" name="agreeTerms" autocomplete="off" value="agree">
-              <label class="btn btn-outline-primary" for="agreeTerms">
-                Zatwierdzam <a href="#">regulamin</a>
-              </label>
             </div>
+            </div>
+            <input type="text" class="form-control my-2" placeholder="Dodaj komentarz" name="comments">
 
-            <button type="submit" class="btn btn-primary m-4">Zamów</button>
+            <div class="d-flex flex-column flex-lg-row w-100 justify-content-center">
+              <div class="col col-lg-4 btn-group m-2" role="group" aria-label="Basic radio toggle button group">
+                <input type="checkbox" class="btn-check" id="agreeTerms" name="agreeTerms" autocomplete="off" value="agree">
+                <label class="btn btn-outline-primary" for="agreeTerms">
+                  Zatwierdzam <a href="#" class="text-reset text-decoration-none fw-bolder">regulamin</a>
+                </label>
+              </div>
+
+              <button type="submit" class="col col-lg-6 btn btn-primary m-2">Zamów</button>
+            </div>
           </form>
-
-          <h1>Produkty<h1>
+          <br>
+          <h3 class="px-4">Produkty:</h3>
+          <div class='m-2 d-flex flex-wrap flex-column flex-lg-row'>
           <?php
             if(isset($_SESSION['cart'])) {
               $cart_value = 0;
@@ -100,19 +113,19 @@ DELIVERY_METHOD;
                 $final_price = intval($value) * intval($product['price']);
                 $cart_value += $final_price;
                 echo <<< INFO
-                <div class="d-flex flex-column flex-lg-row">
-                  <h3>$product[name]</h3>
-                  <p>$value</p>
-                  <p>$product[price] zł</p>
-                  <p>$final_price zł</p>
+                <div class="p-4 m-2 bg-info bg-gradient d-flex flex-column rounded">
+                  <h5>Nazwa: $product[name]</h5>
+                  <h5>Ilość: $value</h5>
+                  <h5>Cena za sztukę: $product[price] zł</h5>
+                  <h5>Cena końcowa: $final_price zł</h5>
                 </div>
 INFO;
               }
               $_SESSION['cart_value'] = $cart_value;
-              echo "<h7>Cena końcowa: $_SESSION[cart_value] zł</h7>";
             }
-          ?>
-          </table>
+            ?>
+          </div>
+          <?php echo "<h4 class='px-4'>Cena końcowa: $_SESSION[cart_value] zł</h4>" ?>
         </div>
       <?php endif ?>
     </div>
--- views/order-details.php
+++ views/order-details.php
@@ -10,7 +10,7 @@
     <?php require_once '../style/links.php'; ?>
   </head>
   <body>
-    <div class="container-fluid w-100 bg-dark screen-height d-flex justify-content-center align-items-center text-light menu-buffer">
+    <div class="container-fluid w-100 bg-dark screen-height d-flex justify-content-center align-items-center text-light menu-buffer py-4">
       <div class="col col-lg-6 bg-warning bg-gradient rounded d-flex flex-column justify-content-center mx-1 my-4 p-4">
         <?php if (isset($_SESSION['success'])) : ?>
           <?php
@@ -26,16 +26,18 @@
               $order = $result->fetch_assoc();
 
               echo <<< INFO
-                <h3 class="p-4 bg-primary bg-gradient rounded">Numer: $order[number]</h3><br>
+                <h3 class="p-4 m-0 bg-primary bg-gradient rounded">Numer: $order[number]</h3>
+                <br>
+                <h5 class="px-4">Utworzono: $order[created_at]</h5>
                 <div class="d-flex flex-column flex-lg-row">
-                  <div class="col-6 p-4">
+                  <div class="col col-lg-6 p-4 border-top border-white">
                     <h3>Dane</h3>
                     <h5>Imię: $order[name]</h5>
                     <h5>Nazwisko: $order[surname]</h5>
                     <h5>Email: $order[email]</h5>
                     <h5>Telefon: $order[phone]</h5>
                   </div>
-                  <div class="col-6 p-4">
+                  <div class="col col-lg-6 p-4 border-top border-white">
                     <h3>Adres</h3>
                     <h5>Kod pocztowy: $order[zipcode]</h5>
                     <h5>Miasto: $order[city]</h5>
@@ -43,40 +45,40 @@
                     <h5>Budynek/mieszkanie: $order[apartment]</h5>
                   </div>
                 </div>
-                <div class="d-flex flex-column flex-lg-row">
-                  <h5>Wartość zamówienia: $order[total_price] zł</h5><br>
-                  <h5>Utworzono: $order[created_at]</h5><br>
-                </div>
+                <h3 class="px-4 py-2 border-top border-white w-100">Wartość zamówienia: $order[total_price] zł</h3>
+                <h3 class="px-4">Produkty:</h3>
 INFO;
               $products = json_decode($order['products'], false);
+              echo "<div class='m-2 d-flex flex-wrap flex-column flex-lg-row'>";
               foreach ($products as $product) {
                 $sql = "SELECT name, price FROM `products` WHERE id = $product->product_id";
                 $result = $mysqli->query($sql);
                 $product_data = $result->fetch_assoc();
                 $final_price = intval($product->quantity) * intval($product_data['price']);
                 echo <<<INFO
-                <div class="d-flex flex-column flex-lg-row">
-                  <h3>$product_data[name]</h3>
-                  <p>$product->quantity</p>
-                  <p>$product_data[price]</p>
-                  <p>$final_price</p>
+                <div class="p-4 m-2 bg-info bg-gradient d-flex flex-column rounded">
+                  <h5>Nazwa: $product_data[name]</h5>
+                  <h5>Ilość: $product->quantity</h5>
+                  <h5>Cena za sztukę: $product_data[price] zł</h5>
+                  <h5>Cena końcowa: $final_price zł</h5>
                 </div>
 INFO;
+                }
               }
-            }
+            echo "</div>";
             if (isset($_SESSION['user_role'])) {
               if($_SESSION['user_role'] == 'employee' && $order['status'] == 0) {
                 echo <<< OPTIONS
-                <div class="d-flex flex-column flex-lg-row">
-                  <form action="../scripts/accept-order.php" method="post">
+                <div class="d-flex flex-column flex-lg-row w-100 justify-content-center">
+                  <form action="../scripts/accept-order.php" method="post" class="col col-lg-4 mx-4 my-1">
                     <input type="text" value="accept" hidden="true" name="decision" />
                     <input type="text" value="$_GET[number]" hidden="true" name="number" />
-                    <button type="submit" class="btn btn-primary m-4">Zaakceptuj</button>
+                    <button type="submit" class="w-100 btn btn-success">Zaakceptuj</button>
                   </form>
-                  <form action="../scripts/accept-order.php" method="post">
+                  <form action="../scripts/accept-order.php" method="post" class="col col-lg-4 mx-4 my-1">
                     <input type="text" value="reject" hidden="true" name="decision" />
                     <input type="text" value="$_GET[number]" hidden="true" name="number" />
-                    <button type="submit" class="btn btn-primary m-4">Odrzuć</button>
+                    <button type="submit" class="w-100 btn btn-danger">Odrzuć</button>
                   </form>
                 </div>
 OPTIONS;
